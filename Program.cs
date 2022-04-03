4ef1d019bcf8797fa4e817525c26c5f3cc9c7767
Nat Sl <slupitska@gmail.com>
2022-04-04 00:49:40 +0300

    add my third.4 project

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение просит пользователя ввести результат его экзамена
+            //результат экзамена - это одна буква от A до F
+            //A - Outstanding
+            //B - Superior
+            //C - Good
+            //D - Satisfactory
+            //E - Low Pass
+            //F - Failure
+            //в работе приложения используется switch
+
+            Console.WriteLine("Enter your exam result:");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+                        
+            switch (userInput)
+            {
+                case 'A':
+                    Console.WriteLine("Outstanding!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Superior!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory.");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low Pass.");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure.");
+                    break;
+                default:
+                    Console.WriteLine($"Incorrect data {userInput}. Please try again.");
+                    break;
+            }
+        }
+    }
+}
